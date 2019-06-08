4f21428dfaac1a9bcd3e49bdb208b3869cc383e1
Cas Eliëns <cas.eliens@gmail.com>
2019-06-08 18:35:29 +0200

    Really remove reports for real this time

--- app.js
+++ app.js
@@ -134,7 +134,6 @@ function create(env, ctx) {
         "/profile":"profileindex.html",
         "/food":"foodindex.html",
         "/bgclock.html":"bgclock.html",
-        "/report":"reportindex.html",
         "/translations":"translationsindex.html",
         "/clock.html":"clock.html"
     };
